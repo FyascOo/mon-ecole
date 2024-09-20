2701c0763385b880d3fc6becaddd47c29054510f
Fyasco <alan.choufa@gmail.com>
2024-09-20 22:55:00 +0200

    feat: pwa

--- apps/mon-ecole-front/src/app/app.config.ts
+++ apps/mon-ecole-front/src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes), provideHttpClient()],
+  providers: [provideRouter(appRoutes), provideHttpClient(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })],
 };
